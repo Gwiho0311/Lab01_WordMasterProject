27bb14d181b07ae2a800cbffce93b49f9af38dc9
Cho, Gwiho <godj0311@handong.ac.kr>
2023-09-05 01:28:58 +0900

    Main class에 주석 추가

--- src/main/java/com/mycom/word/Main.java
+++ src/main/java/com/mycom/word/Main.java
@@ -3,6 +3,6 @@ package com.mycom.word;
 public class Main { // main class
     public static void main(String[] args) {
 
-        new WordManager().start();
+        new WordManager().start(); // 시작
     }
 }
