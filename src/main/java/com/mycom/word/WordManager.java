b618c013759f4f573e076bbeb0b2c764ea4b9597
Cho, Gwiho <godj0311@handong.ac.kr>
2023-09-05 10:11:10 +0900

    Class WordManager에 주석 추가

--- src/main/java/com/mycom/word/WordManager.java
+++ src/main/java/com/mycom/word/WordManager.java
@@ -29,32 +29,32 @@ public class WordManager {
         while(true){
             int menu = selectMenu();
             System.out.println(menu);
-            if(menu == 0) break;
-            if(menu == 4){
+            if(menu == 0) break; // 프로그램 종료: 0
+            if(menu == 4){ // 단어 추가: 4
                 wordCRUD.addItem();
 
             }
-            else if(menu == 1){
+            else if(menu == 1){ // 모든 단어보기: 1
                 wordCRUD.listAll();
 
             }
-            else if(menu == 2){
+            else if(menu == 2){ // 수준별 단어보기: 2
                 wordCRUD.searchLevel();
 
             }
-            else if(menu == 3){
+            else if(menu == 3){ // 단어 검색: 3
                 wordCRUD.searchWord();
 
             }
-            else if(menu == 5){
+            else if(menu == 5){ // 단어 수정: 5
                 wordCRUD.updateItem();
 
             }
-            else if(menu == 6){
+            else if(menu == 6){ // 단어 삭제: 6
                 wordCRUD.deleteItem();
 
             }
-            else if(menu == 7){
+            else if(menu == 7){ // 파일 저장: 7
                 wordCRUD.saveFile();
             }
 
